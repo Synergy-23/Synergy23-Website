c339ad489b8904b0f169dbe6c104899d9de30544
brij <brijdesai2003@gmail.com>
2022-10-13 00:28:39 +0530

    Fix accordion responsiveness

--- src/components/Faq.jsx
+++ src/components/Faq.jsx
@@ -1,19 +1,28 @@
 import Accordion from 'react-bootstrap/Accordion';
+import { useState } from 'react';
 import { Container } from 'react-bootstrap';
 import faqs from '../content/faq';
 import "./Faq.scss";
 const Faq = () => {
+    const [flag, setFlag] = useState(false);
+    
+    const toggleFlag = () => {
+        setFlag(current => !current);
+    }
+    
     const renderFaqs = (faqs) => {
         return faqs.map((faq, index) => {
             return (
                 <div className='accordion-content'>
-                    <input type="radio"  name="example_accordion" id={`section${index}`} className="accordion__input"></input>
+                    <input type="radio"  name="example_accordion" id={`section${index}`} className="accordion__input" onClick = {toggleFlag}></input>
                     <label for={`section${index}`} className="accordion__label">{faq.question}</label>
+                    {flag && (
                     <div className="accordion__content">
                         <p>
                             {faq.answer}
                         </p>
                     </div>
+                    )}
                 </div>
             );
         });
