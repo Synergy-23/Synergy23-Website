8044c9a941a963603079329e9899850fcd06df5c
RohitShah1706 <rlshah03@gmail.com>
2022-10-31 20:28:58 +0530

    fixed card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,5 +1,6 @@
 import "./Card.scss";
-
+import { BsLinkedin } from "react-icons/bs";
+import { Row,Col } from "react-bootstrap";
 
 const Card = (props) => {
     return (
@@ -7,7 +8,19 @@ const Card = (props) => {
             <div className="card-main">
                 <div className="cont">
                     <img className="cardimg" src={props.img} alt="" />
-                    <p>{props.content}</p>
+                    <Row>
+                        <Col xs={9}>
+                            <p className="team-title">{props.content}</p>
+                        </Col>
+                        <Col xs={3}>
+                            <a href="https://www.linkedin.com/company/synergy-iiitbangalore/" target="_blank" rel="noreferrer" className="footer-icon">
+                                <BsLinkedin style={{ color: "white", fontSize: "2em" }} className="ra-icon" />
+                            </a>
+                        </Col>
+                    </Row>
+                    <div className="second-name">
+                        <p className="team-subtitle">Organizer</p>
+                    </div>
                 </div>
             </div>
         </>
