14727a848acfc3875732a2b743ef1763d54074ad
Subhajeet Lahiri <subhajeetlahiri@gmail.com>
2022-11-01 18:32:15 +0530

    fix for about us on md screens

--- src/components/big_side_heading.jsx
+++ src/components/big_side_heading.jsx
@@ -2,7 +2,7 @@ import './big_side_heading.scss';
 const Big_side_heading = (props)=>{
     return <div className="big_side_heading container">
         <div className="row">
-            <div className="col-12 col-md-4 heading_column">
+            <div className="col-12 col-md-3 heading_column">
                 {/* for smaller screens */}
                 <div className="row d-block d-md-none">
                     <h1 className="small_screen_heading">
@@ -10,21 +10,20 @@ const Big_side_heading = (props)=>{
                         <span className='dot_color'>.</span>
                     </h1>
                 </div>
-                <div className="row d-none d-md-block">
-                    <div className="row">
+                <div className="row d-none d-md-block sideheadingrow">
+                    <div className="row sideheadingrow">
                         <h1 className="bs_heading_1">
                         {props.heading1}
                         </h1>
                     </div>
-                    <div className="row">
+                    <div className="row sideheadingrow">
                        <h1 className="bs_heading_2">
-                       <div className="col"> {props.heading2.toUpperCase()}&nbsp;
-                    </div>
+                        {props.heading2.toUpperCase()}&nbsp;
                        </h1>
                     </div>
                 </div>
             </div>
-            <div className="col-12 col-md-8 content_style">
+            <div className="col-12 col-md-6 offset-md-2 content_style">
                 {props.content}
             </div>
         </div>
