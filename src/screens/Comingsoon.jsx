a42473d53a8a418e80336d8908374426b840829e
Subhajeet Lahiri <subhajeetlahiri@gmail.com>
2022-11-05 21:23:24 +0530

    small change in breakpoints

--- src/screens/Comingsoon.jsx
+++ src/screens/Comingsoon.jsx
@@ -17,38 +17,38 @@ function Comingsoon(){
                     <h1 className='synergy_is_text1'>COMING SOON</h1>
                 </div>
             </div>
-            <div className="col d-none d-md-block">
+            <div className="col d-none d-lg-block">
             </div>
         </div> */}
         <div className="row">
             <div className="empty_space_title1"></div>
-            <div className="col-1 col-md-2"></div>
-            <div className="col-11 col-md-4">
+            <div className="col-1 col-lg-2"></div>
+            <div className="col-11 col-lg-4">
                 <h1 className="topcoming">Synergy 2023 is</h1>
             </div>
-            <div className="col d-none d-md-block"> </div>
+            <div className="col d-none d-lg-block"> </div>
         </div>
         <div className="row">
-            <div className="col-2 d-none d-md-block"></div>
-            <div className="col-12 col-md-4">
+            <div className="col-2 d-none d-lg-block"></div>
+            <div className="col-12 col-lg-4">
                 <h1 className='synergy_is_text1 textcenter'>COMING</h1>
             </div>
-            <div className="col-4 d-none d-md-block">
+            <div className="col-4 d-none d-lg-block">
                 <h1 className='synergy_is_text2'>SOON</h1>
             </div>
-            <div className="col-2 d-none d-md-block"></div>
+            <div className="col-2 d-none d-lg-block"></div>
         </div>
-        <div className="row d-block d-md-none">
+        <div className="row d-block d-lg-none">
             <h1 className='synergy_is_text2 textcenter'>SOON</h1>
         </div>
         <div className="row">   
-            <div className="col-4 col-md-6"></div>
-            <div className="col-7 col-md-4 desc">2nd iteration of IIITB's</div>
+            <div className="col-4 col-lg-6"></div>
+            <div className="col-7 col-lg-4 desc">2nd iteration of IIITB's</div>
             <div className="col-2"></div>
         </div>
         <div className="row">   
-            <div className="col-4 col-md-6"></div>
-            <div className="col-7 col-md-4 desc">Annual TechFest</div>
+            <div className="col-4 col-lg-6"></div>
+            <div className="col-7 col-lg-4 desc">Annual TechFest</div>
             <div className="col-2"> </div>
         </div>
         <div className="row empty_space_title1">
@@ -57,28 +57,28 @@ function Comingsoon(){
             <h6 className ="textcenter contactuscom">Contact us here</h6>
         </div>
         <div className="row textcenter socialrow mobilesocial">
-            <div className="col-2 col-md"></div>
-            <div className="col-2 col-md-1">
+            <div className="col-2 col-lg"></div>
+            <div className="col-2 col-lg-1">
             <a href="https://www.linkedin.com/company/synergy-iiitbangalore/" target="_blank" rel="noreferrer" className="footer-icon socialrow">
                 <BsLinkedin style={{ color: "white", fontSize: "2.3em" }} className="ra-icon" />
             </a>
             </div>
-            <div className="col-2 col-md-1">
+            <div className="col-2 col-lg-1">
             <a href="https://www.instagram.com/synergy_iiitb/" target="_blank" rel="noreferrer" className="footer-icon socialrow">
                 <AiFillInstagram style={{ color: "white", fontSize: "2.5em" }} className="ra-icon" />
             </a>
             </div>
-            <div className="col-2 col-md-1">
+            <div className="col-2 col-lg-1">
             <a href="https://www.facebook.com/IIITBofficial/" target="_blank" rel="noreferrer" className="footer-icon socialrow">
                 <BsFacebook style={{ color: "white", fontSize: "2.3em" }} className="ra-icon" />
             </a>
             </div>
-            <div className="col-2 col-md-1">
+            <div className="col-2 col-lg-1">
             <a href="https://twitter.com/IIITB_official" target="_blank" rel="noreferrer" className="footer-icon socialrow">
                 <FaTwitter style={{ color: "white", fontSize: "2.3em" }} className="ra-icon" />
             </a>
             </div>
-            <div className="col-2 col-md"></div>
+            <div className="col-2 col-lg"></div>
         </div>
     </div>
 }
